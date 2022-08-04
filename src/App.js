cbca63ab08b8717e29cf79a66a3482acfee392f0
Raj Sankhavara <raj.sankhavara.kevit@gmail.com>
2022-08-04 15:21:06 +0530

    Use Reducer Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useReducer, useState } from "react"
 
 function App() {
+  function reducer(todos, action) {
+    switch (action.type) {
+      case "add-todo":
+        return [...todos, action.payload]
+      case "toggle-todo":
+        return todos.map(todo => {
+          if (todo.id === action.payload.id)
+            return { ...todo, completed: !todo.completed }
+          return todo;
+        })
+      case "delete-todo":
+        return todos.filter(todo => todo.id !== action.payload.id)
+      default:
+        return todos
+    }
+  }
+
+  const [todos, dispatch] = useReducer(reducer, [])
+  const [value, setValue] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: "add-todo", payload: { id: Date.now(), name: value, completed: false } })
+    setValue("");
+  }
+
+  const toggleTodo = (id) => {
+    dispatch({ type: "toggle-todo", payload: { id } })
+  }
+
+  const deleteTodo = (id) => {
+    dispatch({ type: "delete-todo", payload: { id } })
+  }
+
+
   return (
     <>
-      <h1>Change code here</h1>
+      <h1>Your TODOS</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+        <button type="submit">add</button>
+      </form>
+      {
+        todos.map(todo => {
+          return (
+            <div>
+              <p style={{ color: todo.completed ? "gray" : "black" }}>{todo.name}</p>
+              <button onClick={() => toggleTodo(todo.id)}>Toggle</button>
+              <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </div>
+          )
+        })
+      }
     </>
   )
 }
 
-export default App;
+export default App
